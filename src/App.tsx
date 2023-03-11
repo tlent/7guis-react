ba547a71a92e6842759b4f506012e799576f9afa
Thomas Lent <tlent@pm.me>
2023-03-11 16:43:06 -0500

    Simplify App

--- src/App.tsx
+++ src/App.tsx
@@ -10,65 +10,45 @@ import {
   Cells,
 } from "./GUIs";
 
-const GuiComponents: { [name: string]: JSX.Element } = {
-  Counter: <Counter />,
-  "Temperature Converter": <TemperatureConverter />,
-  "Flight Booker": <FlightBooker />,
-  Timer: <Timer />,
-  CRUD: <Crud />,
-  "Circle Drawer": <CircleDrawer />,
-  Cells: <Cells />,
-};
-
-const GuiNames = Object.keys(GuiComponents);
+const GUIs = [
+  { name: "Counter", component: <Counter /> },
+  { name: "Temperature Converter", component: <TemperatureConverter /> },
+  { name: "Flight Booker", component: <FlightBooker /> },
+  { name: "Timer", component: <Timer /> },
+  { name: "CRUD", component: <Crud /> },
+  { name: "Circle Drawer", component: <CircleDrawer /> },
+  { name: "Cells", component: <Cells /> },
+];
 
 export default function App() {
-  const [selectedGui, setSelectedGui] = useState(GuiNames[0]);
-  const content = GuiComponents[selectedGui];
+  const [selectedGui, setSelectedGui] = useState(GUIs[0]);
 
-  return (
-    <main className="py-4 md:container md:mx-auto">
-      <PageHeading />
-      <GuiSelector onSelect={(guiName) => setSelectedGui(guiName)} />
-      <div className="m-4 mx-auto w-fit p-4">{content}</div>
-    </main>
-  );
-}
-
-function PageHeading() {
-  return (
-    <h1 className="mb-5 p-4 text-center text-5xl font-semibold text-neutral-700">
-      <a
-        href="https://eugenkiss.github.io/7guis/"
-        className="text-blue-700 underline"
-      >
-        7GUIs
-      </a>{" "}
-      with React
-    </h1>
-  );
-}
-
-function GuiSelector({ onSelect }: { onSelect: (guiName: string) => void }) {
   function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    const name = event.target.value;
-    onSelect(name);
+    setSelectedGui(GUIs[event.target.selectedIndex]);
   }
 
   return (
-    <div className="mx-auto w-fit">
-      <label htmlFor="selectedGUI" className="sr-only">
-        Select a GUI
-      </label>
-      <select name="selectedGUI" onChange={handleChange}>
-        {GuiNames.map((name) => {
-          return (
-            <option key={name} value={name}>
-              {name}
-            </option>
-          );
-        })}
-      </select>
-    </div>
+    <main className="py-4 md:container md:mx-auto">
+      <h1 className="mb-5 p-4 text-center text-5xl font-semibold">
+        <a
+          href="https://eugenkiss.github.io/7guis/"
+          className="text-blue-700 underline"
+        >
+          7GUIs
+        </a>{" "}
+        with React
+      </h1>
+      <div className="mx-auto w-fit">
+        <label htmlFor="selectedGUI" className="sr-only">
+          Select a GUI
+        </label>
+        <select name="selectedGUI" onChange={handleChange}>
+          {GUIs.map(({ name }) => {
+            return <option key={name}>{name}</option>;
+          })}
+        </select>
+      </div>
+      <div className="m-4 mx-auto w-fit p-4">{selectedGui.component}</div>
+    </main>
   );
 }
