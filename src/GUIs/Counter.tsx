17a867003db935c24e9a79acbb6409432df21451
Thomas Lent <tlent@pm.me>
2023-03-11 16:28:58 -0500

    Add styles to Counter

--- src/GUIs/Counter.tsx
+++ src/GUIs/Counter.tsx
@@ -5,8 +5,13 @@ export default function Counter() {
 
   return (
     <>
-      <div>{count}</div>
-      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <div className="mb-3 text-center text-5xl">{count}</div>
+      <button
+        onClick={() => setCount((c) => c + 1)}
+        className="mx-auto w-24 rounded-full border border-neutral-200 px-4 py-1 text-sm font-semibold text-neutral-600 hover:border-transparent hover:bg-neutral-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-600"
+      >
+        +1
+      </button>
     </>
   );
 }
