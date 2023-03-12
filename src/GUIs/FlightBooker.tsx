d90f56bc66f9905fbbbd0cd69a64397a853e99e8
Thomas Lent <tlent@pm.me>
2023-03-12 14:53:40 -0400

    Implement FlightBooker

--- src/GUIs/FlightBooker.tsx
+++ src/GUIs/FlightBooker.tsx
@@ -1,3 +1,73 @@
+import { useState } from "react";
+
+import Button from "../components/Button";
+
+enum FlightType {
+  OneWay = "One Way",
+  Return = "Return",
+}
+
 export default function FlightBooker() {
-  return <p>FlightBooker</p>;
+  const today = new Date().toISOString().split("T")[0];
+
+  const [flightType, setFlightType] = useState<FlightType>(FlightType.OneWay);
+  const [departureDate, setDepartureDate] = useState<string>(today);
+  const [returnDate, setReturnDate] = useState<string | null>(null);
+
+  const isValid =
+    departureDate >= today &&
+    (flightType !== FlightType.Return ||
+      (returnDate && returnDate >= departureDate));
+
+  function handleFlightTypeChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const flightType = event.target.value as FlightType;
+    setFlightType(flightType);
+    if (flightType === FlightType.OneWay) {
+      setReturnDate(null);
+    } else if (flightType === FlightType.Return) {
+      setReturnDate(departureDate);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="flightTypeSelect">Flight Type</label>
+        <select
+          id="flightTypeSelect"
+          value={flightType}
+          onChange={handleFlightTypeChange}
+        >
+          {Object.values(FlightType).map((type) => (
+            <option key={type}>{type}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="departureDate">Departure Date</label>
+        <input
+          className="invalid:border-4 invalid:border-red-600"
+          id="departureDate"
+          type="date"
+          value={departureDate}
+          min={today}
+          onChange={(e) => setDepartureDate(e.target.value)}
+        />
+      </div>
+      {flightType === FlightType.Return && (
+        <div>
+          <label htmlFor="returnDate">Return Date</label>
+          <input
+            className="invalid:border-4 invalid:border-red-600"
+            id="returnDate"
+            type="date"
+            value={returnDate || ""}
+            min={departureDate}
+            onChange={(e) => setReturnDate(e.target.value)}
+          />
+        </div>
+      )}
+      <Button disabled={!isValid}>Book</Button>
+    </>
+  );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ className = "", ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={`w-24 rounded-full border border-neutral-200 px-4 py-1 text-sm font-semibold text-neutral-600 hover:border-transparent hover:bg-neutral-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-600 ${className}`}
+      className={`w-24 rounded-full border border-neutral-200 px-4 py-1 text-sm font-semibold text-neutral-600 focus:outline-none focus:ring-2 focus:ring-blue-600 enabled:hover:border-transparent enabled:hover:bg-neutral-600 enabled:hover:text-white disabled:bg-neutral-400 ${className}`}
     ></button>
   );
 }
