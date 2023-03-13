9d316d17be7c22daf30d07eef4072d6964b13277
Thomas Lent <tlent@pm.me>
2023-03-13 14:30:00 -0400

    Implement initial functionality for Timer

--- src/GUIs/Timer.tsx
+++ src/GUIs/Timer.tsx
@@ -1,3 +1,51 @@
-export default function Timer() {
-  return <p>Timer</p>;
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+
+export default function Timer({
+  initialTimerDuration = 10,
+  updateInterval = 100,
+}: {
+  initialTimerDuration: number;
+  updateInterval: number;
+}) {
+  const [timerDuration, setTimerDuration] = useState(initialTimerDuration);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  const timerShouldRun = elapsedTime < timerDuration;
+
+  useEffect(() => {
+    if (!timerShouldRun) {
+      return;
+    }
+
+    let prevTickTime = Date.now();
+    const id = setInterval(tick, updateInterval);
+
+    function tick() {
+      const now = Date.now();
+      const dt = (now - prevTickTime) / 1000;
+      prevTickTime = now;
+      setElapsedTime((t) => {
+        const newElapsedTime = t + dt;
+        if (newElapsedTime > timerDuration) {
+          clearInterval(id);
+          return timerDuration;
+        }
+        return newElapsedTime;
+      });
+    }
+
+    return () => {
+      tick();
+      clearInterval(id);
+    };
+  }, [updateInterval, timerDuration, timerShouldRun]);
+
+  return (
+    <>
+      <p>Elapsed time: {elapsedTime}</p>
+      <p>Timer duration: {timerDuration}</p>
+      <Button onClick={() => setElapsedTime(0)}>Reset</Button>
+    </>
+  );
 }
