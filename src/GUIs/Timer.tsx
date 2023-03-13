23c1f275ede2f4d546799bed08645e73a05c1423
Thomas Lent <tlent@pm.me>
2023-03-13 17:18:09 -0400

    Use useReducer

--- src/GUIs/Timer.tsx
+++ src/GUIs/Timer.tsx
@@ -1,18 +1,59 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useReducer } from "react";
 import Button from "../components/Button";
+
+enum ActionType {
+  Tick = "tick",
+  SetDuration = "set_duration",
+  Reset = "reset",
+}
+
+type Action =
+  | {
+      type: ActionType.Tick;
+      deltaTime: number;
+    }
+  | {
+      type: ActionType.SetDuration;
+      newTimerDuration: number;
+    }
+  | { type: ActionType.Reset };
+
+type State = {
+  elapsedTime: number;
+  timerDuration: number;
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case ActionType.Tick:
+      return {
+        ...state,
+        elapsedTime: Math.min(
+          state.elapsedTime + action.deltaTime,
+          state.timerDuration
+        ),
+      };
+    case ActionType.SetDuration:
+      return {
+        ...state,
+        timerDuration: action.newTimerDuration,
+        elapsedTime: Math.min(state.elapsedTime, action.newTimerDuration),
+      };
+    case ActionType.Reset:
+      return { ...state, elapsedTime: 0 };
+  }
+}
 
 export default function Timer({
   initialTimerDuration = 10,
 }: {
   initialTimerDuration?: number;
 }) {
-  const [elapsedTime, setElapsedTime] = useState(0);
-  const [timerDuration, setTimerDuration] = useState(initialTimerDuration);
-
-  const timerDurationRef = useRef(timerDuration);
-  timerDurationRef.current = timerDuration;
-
-  const timerShouldRun = elapsedTime < timerDuration;
+  const [state, dispatch] = useReducer(reducer, {
+    elapsedTime: 0,
+    timerDuration: initialTimerDuration,
+  });
+  const timerShouldRun = state.elapsedTime < state.timerDuration;
 
   useEffect(() => {
     if (!timerShouldRun) {
@@ -22,8 +63,8 @@ export default function Timer({
     let id = requestAnimationFrame(tick);
 
     function tick(timestamp: DOMHighResTimeStamp) {
-      const dt = timestamp - prevTickTimestamp;
-      setElapsedTime((t) => Math.min(t + dt / 1000, timerDurationRef.current));
+      const deltaTime = (timestamp - prevTickTimestamp) / 1000;
+      dispatch({ type: ActionType.Tick, deltaTime });
       prevTickTimestamp = timestamp;
       id = requestAnimationFrame(tick);
     }
@@ -37,26 +78,30 @@ export default function Timer({
     <div className="w-40">
       <progress
         className="h-4 w-full overflow-hidden rounded-md"
-        max={timerDuration}
-        value={elapsedTime}
+        max={state.timerDuration}
+        value={state.elapsedTime}
       ></progress>
       <p className="mt-1">
-        Elapsed time: {Math.round(elapsedTime * 100) / 100}s
+        Elapsed time: {Math.round(state.elapsedTime * 100) / 100}s
       </p>
       <input
         className="mt-4"
         type="range"
-        value={timerDuration}
+        value={state.timerDuration}
         min={1}
         max={60}
         onChange={(e) => {
-          const newTimerDuration = e.target.valueAsNumber;
-          setTimerDuration(newTimerDuration);
-          setElapsedTime((t) => Math.min(t, newTimerDuration));
+          dispatch({
+            type: ActionType.SetDuration,
+            newTimerDuration: e.target.valueAsNumber,
+          });
         }}
       />
-      <p className="mt-1">Timer duration: {timerDuration}s</p>
-      <Button className="mt-4 w-full" onClick={() => setElapsedTime(0)}>
+      <p className="mt-1">Timer duration: {state.timerDuration}s</p>
+      <Button
+        className="mt-4 w-full"
+        onClick={() => dispatch({ type: ActionType.Reset })}
+      >
         Reset
       </Button>
     </div>
