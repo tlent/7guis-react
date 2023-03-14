2b18b53a74f4a6e2f67ceaf56c8049dd9ed901bf
Thomas Lent <tlent@pm.me>
2023-03-14 14:16:10 -0400

    Improve linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+  ],
+  ignorePatterns: ["*.config.ts", "*.config.cjs", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
