40ca0943b0fc58260f4e64adf9abac892993a431
Thomas Lent <tlent@pm.me>
2023-03-14 17:23:43 -0400

    Improve style

--- src/GUIs/Crud.tsx
+++ src/GUIs/Crud.tsx
@@ -4,16 +4,12 @@ export default function Crud() {
   const names = ["Hello", "Hi", "Hiya"];
   return (
     <div className="space-y-3 rounded border border-neutral-400 px-10 py-5 shadow">
-      <div>
+      <div className="space-x-2">
         <label htmlFor="filter-prefix">Filter prefix:</label>
-        <input
-          type="text"
-          id="filter-prefix"
-          className="ml-2 h-8 w-32 rounded"
-        />
+        <input type="text" id="filter-prefix" className="h-8 w-32 rounded" />
       </div>
-      <div className="flex">
-        <div className="mr-4 w-48">
+      <div className="flex space-x-4">
+        <div className="w-48">
           <select
             size={names.length}
             className="form-multiselect h-32 w-full bg-none"
@@ -23,17 +19,13 @@ export default function Crud() {
             ))}
           </select>
         </div>
-        <div>
-          <div className="mb-2 flex justify-between">
-            <label htmlFor="name" className="mr-4">
-              Name:
-            </label>
-            <input type="text" id="name" className="ml-auto h-8 w-32 rounded" />
+        <div className="space-y-2">
+          <div className="flex justify-between space-x-3">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" className="h-8 w-32 rounded" />
           </div>
-          <div className="flex justify-between">
-            <label htmlFor="surname" className="mr-4">
-              Surname:
-            </label>
+          <div className="flex justify-between space-x-3">
+            <label htmlFor="surname">Surname:</label>
             <input type="text" id="surname" className="h-8 w-32 rounded" />
           </div>
         </div>
