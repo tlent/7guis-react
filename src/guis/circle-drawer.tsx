61ce2110ac192ef4bcce1e11d2d9cbc4db8d0579
Thomas Lent <tlent@pm.me>
2023-03-16 12:37:39 -0400

    Initial layout for Circle Drawer

--- src/guis/circle-drawer.tsx
+++ src/guis/circle-drawer.tsx
@@ -1,3 +1,13 @@
+import Button from "../components/button";
+
 export default function CircleDrawer() {
-  return <p>CircleDrawer</p>;
+  return (
+    <div className="space-y-4 rounded border border-neutral-400 p-6 shadow">
+      <div className="flex justify-center space-x-2">
+        <Button>Undo</Button>
+        <Button>Redo</Button>
+      </div>
+      <div className="h-64 w-96 border border-black"></div>
+    </div>
+  );
 }
