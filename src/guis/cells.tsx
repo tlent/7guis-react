49e2e7e3b9391d02a59df806fc4339d092f37645
Thomas Lent <tlent@pm.me>
2023-03-17 15:43:24 -0400

    Add basic table for Cells

--- src/guis/cells.tsx
+++ src/guis/cells.tsx
@@ -1,3 +1,31 @@
+const ALPHABET = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
 export default function Cells() {
-  return <p>Cells</p>;
+  return (
+    <table className="h-96 w-96 overflow-scroll">
+      <thead>
+        <tr>
+          <th></th>
+          {ALPHABET.map((letter) => (
+            <th key={letter}>{letter}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {range(0, 100).map((rowNumber) => (
+          <tr key={rowNumber}>
+            <th>{rowNumber}</th>
+            {range(0, 26).map((columnNumber) => (
+              <td key={columnNumber}>{columnNumber}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function range(start: number, end: number): readonly number[] {
+  const length = end - start;
+  return [...Array.from({ length }).keys()].map((index) => index + start);
 }
