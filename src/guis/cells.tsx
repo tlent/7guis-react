3e72e74610208433919ed1a8029afa59aaaf9a77
Thomas Lent <tlent@pm.me>
2023-03-18 15:15:48 -0400

    Improve table layout

--- src/guis/cells.tsx
+++ src/guis/cells.tsx
@@ -2,26 +2,30 @@ const ALPHABET = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 export default function Cells() {
   return (
-    <table className="h-96 w-96 overflow-scroll">
-      <thead>
-        <tr>
-          <th></th>
-          {ALPHABET.map((letter) => (
-            <th key={letter}>{letter}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {range(0, 100).map((rowNumber) => (
-          <tr key={rowNumber}>
-            <th>{rowNumber}</th>
-            {range(0, 26).map((columnNumber) => (
-              <td key={columnNumber}>{columnNumber}</td>
+    <div className="h-96 w-96 overflow-scroll rounded border border-neutral-400">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            {ALPHABET.map((letter) => (
+              <th key={letter} className="border-b border-black">
+                {letter}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {range(0, 100).map((rowNumber) => (
+            <tr key={rowNumber}>
+              <th className="border-r border-black">{rowNumber}</th>
+              {range(0, 26).map((columnNumber) => (
+                <td key={columnNumber}>{columnNumber}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
