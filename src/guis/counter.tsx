8e3465f7f984ff01096411306170c411167c08b2
Thomas Lent <tlent@pm.me>
2023-03-20 19:05:23 -0400

    Fix Counter layout

--- src/guis/counter.tsx
+++ src/guis/counter.tsx
@@ -6,9 +6,9 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="mx-auto flex w-32 flex-col justify-center rounded border border-neutral-400 p-6 shadow">
       <div className="mb-3 text-center text-5xl">{count}</div>
       <Button onClick={() => setCount((c) => c + 1)}>+1</Button>
-    </>
+    </div>
   );
 }
